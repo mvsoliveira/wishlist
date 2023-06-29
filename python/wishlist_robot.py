0adb154099d7f773e0a9991818d6a3a2b12d5f26
Marcos Oliveira <mvsoliveira@gmail.com>
2023-06-30 00:37:44 +0200

    shuffling writes and read ins stress test

--- python/wishlist_robot.py
+++ python/wishlist_robot.py
@@ -6,6 +6,7 @@ import logging
 import random
 import socket
 import sys
+import time
 
 
 class wishlist_robot(object):
@@ -16,6 +17,7 @@ class wishlist_robot(object):
         log_tree(self.tree, self.logger)
 
     def stress_test(self, nodes=None, N=1000):
+        start_time = time.time()
         # Testing the entire tree if nodes is not defined
         if nodes is None:
             nodes = self.tree
@@ -23,24 +25,31 @@ class wishlist_robot(object):
         nodes = [n for n in nodes if (n.permission == 'rw' and n.is_leaf)]
         # Testing all nodes N times
         for i in range(N):
+            # Shuffling nodes (not in-place) order before writing, in-place (random.shuffle) shuffling was causing problems during writing
+            nodes = random.sample(nodes, len(nodes))
             for node in nodes:
                 # Generating stimulus
-                stimulus = random.randint(0, 2 ** node.width - 1)
+                node.stimulus = random.randint(0, 2 ** node.width - 1)
                 # Writing stimulus
-                node.write(stimulus)
+                node.write(node.stimulus)
+            # Shuffling nodes order before reading
+            nodes = random.sample(nodes, len(nodes))
+            for node in nodes:
                 # Reading node
                 value = node.read()
                 # Checking read value against stimulus for errors
-                if value != stimulus:
-                    node.logger.critical(f'node check error at iteration {i}: {value} (0x{value:08x}), expected: {stimulus}')
+                if value != node.stimulus:
+                    node.logger.critical(f'node check error at iteration {i}: {value} (0x{value:08x}), expected: {node.stimulus}')
                     sys.exit()
-                node.logger.debug(f'{value} (0x{value:08x}) {stimulus}')
+                node.logger.debug(f'{value} (0x{value:08x}) {node.stimulus}')
             # Logging status every 100 iterations
-            if not i % 100:
-                robot.logger.info(f'Stress_test iteration {i+1} out of {N}')
+            if not i % 1000:
+                self.logger.info(f'Stress_test iteration {i+1} out of {N} elapsed {time.time() - start_time} seconds')
+        self.logger.info(f'Stress test with {N} iteration finished in {time.time() - start_time} seconds')
+
 
 
 if __name__ == '__main__':
     robot = wishlist_robot()
     nodes = list(preorder_iter(robot.tree, filter_condition=lambda node: node.is_leaf and node.permission == 'rw' and 'PB_' in node.name))
-    robot.stress_test(nodes,N=10000)
+    robot.stress_test(nodes,N=10000*400)
