0b7f7a5a1e9d5b7446b8e42ec4942952bd54570a
Marcos Oliveira <marcos.oliveira@cern.ch>
2026-03-19 02:21:19 -0400

    0.0.27 fixed instantiation template

--- edawishlist/wishlist.py
+++ edawishlist/wishlist.py
@@ -350,5 +350,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    #for path in ['firmware/l1calogfex_backannotated.yaml', 'firmware/l1calogfex_pkg.vhd', 'firmware/l1calogfex_address_decoder.vhd', 'firmware/l1calogfex_instantiation.vhd', 'examples/L1CaloGfex.yaml']:
-        #os.system(f'scp {path} aiatlas-fw-03.cern.ch:git/register/zfpga/zfpga_top/source/')
